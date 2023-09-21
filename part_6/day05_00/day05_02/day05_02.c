d7642d7162b39ebcf2f750ed6829e0875c399df9
symbolfu <fh_swjt@163.com>
2023-09-21 22:47:12 +0800

    add string function

--- part_6/day05_00/day05_02/day05_02.c
+++ part_6/day05_00/day05_02/day05_02.c
@@ -72,6 +72,7 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 
 void test01()
@@ -87,11 +88,61 @@ void test01()
 }
 
 
+void test02()
+{
+	// count words
+	char string[81];
+	int i, num = 0, flag = 0;
+	char c;
+	gets(string);
+	//printf("%s\n", string);
+
+	for (i = 0; (c = string[i]) != '\0'; i++)
+	{
+		//printf("%c\n", c);
+		if (c == ' ') 
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			num++;
+		}
+	}
+
+	printf("there are %d words in the line.\n", num);
+
+}
+
+
+void test03()
+{
+	char str[3][20];
+	char string[20];
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		gets(str[i]);
+	}
+
+	if (strcmp(str[0], str[1]) > 0)
+		strcpy(string, str[0]);
+	else
+		strcpy(string, str[1]);
+
+	if (strcmp(str[2], string) > 0)
+		strcpy(string, str[2]);
+
+	printf("\nthe larget string is: \n%s\n", string);
+
+}
 
 
 int main()
 {
-	test01();
+	// test01();
+	//test02();
+	test03();
 
 
 	return 0;
